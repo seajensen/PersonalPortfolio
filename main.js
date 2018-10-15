c3095c0d58e3fefa69a7bc321fb4bcdcee9e1415
seajensen <seajensen@gmail.com>
2018-10-15 11:16:50 -0600

    initial JSON experiment

---
+++ main.js
@@ -0,0 +1,7 @@
+fetch("https://pokeapi.co/api/v2/pokemon/92/")
+ .then(function(response) {
+     return response.json();
+ })
+ .then(function(myJson) {
+     console.log(JSON.stringify(myJson));
+ });
