52fc019c1c1c0fe70960afcb9619702a7f9a205c
seajensen <seajensen@gmail.com>
2018-11-12 15:40:41 -0700

    Massive progress on cards

--- main.js
+++ main.js
@@ -37,9 +37,49 @@ import {anime} from "/assets/anime.js"
 
 let animeCards = document.querySelector("#card-div")
 anime.forEach(anime => {
+    let currentID = anime.mal_id;
+
+    let cardWrap = document.createElement("div");
+    cardWrap.className = "cardWrap";
+    cardWrap.id = currentID;
+    animeCards.appendChild(cardWrap);
+
+    let outerWrap = document.getElementById(currentID)
+
+    let currentID2 = anime.mal_id + "x";
+    
+    let cardWrapInt = document.createElement("div");
+    cardWrapInt.className = "cardWrapInt";
+    cardWrapInt.id = currentID2;
+    outerWrap.appendChild(cardWrapInt);
+
+    let innerWrap = document.getElementById(currentID2)
+
+    let currentID3 = anime.mal_id + "y";
+
+    let cardFront = document.createElement("div");
+    cardFront.className = "cardFront";
+    cardFront.id = currentID3;
+    innerWrap.appendChild(cardFront);
+
+    let frontside = document.getElementById(currentID3)
+
     let imgsrc = document.createElement("img");
     imgsrc.src = anime.image_url;
-    animeCards.appendChild(imgsrc);
+    frontside.appendChild(imgsrc);
+
+    let currentID4 = anime.mal_id + "z";
+
+    let cardBack = document.createElement("div");
+    cardBack.className = "cardBack";
+    cardBack.id = currentID4;
+    innerWrap.appendChild(cardBack);
+
+    let backside = document.getElementById(currentID4)
+
+    let cardInfo = document.createElement("h2");
+    cardInfo.textContent = anime.title;
+    backside.appendChild(cardInfo);
 });
 
 
